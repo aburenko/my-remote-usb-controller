9e1d780c6116435c64ba07e581bf21d6dc60bae6
Anton Burenko <burenko@incoretex.de>
2023-08-10 01:27:03 +0200

    Fix

--- services/executer-api/src/main.py
+++ services/executer-api/src/main.py
@@ -24,4 +24,4 @@ async def turn_off():
             status_code=500, detail="Failed to turn device off")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8888, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
